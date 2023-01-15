ad49476f2ee81c5c20c5ce1493f4804707aff8bd
João Matheus Silva Franca <matheussfranca@live.com>
2023-01-15 18:20:32 -0300

    Concluído: Requisito 02

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ app.use(express.json());
 
 const HTTP_OK_STATUS = 200;
 const PORT = '3000';
+const TALKER_NOT_FOUND = {
+  message: 'Pessoa palestrante não encontrada',
+};
 
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
@@ -15,6 +18,18 @@ app.get('/talker', async (req, res) => {
   try {
     const talkers = await fs.readFile('src/talker.json', 'utf-8');
     res.status(200).json(JSON.parse(talkers));
+  } catch (error) {
+    console.error(error);
+  }
+});
+
+app.get('/talker/:id', async (req, res) => {
+  try {
+    const data = await fs.readFile('src/talker.json', 'utf-8');
+    const talkers = JSON.parse(data);
+    const talker = talkers.find((e) => e.id === Number(req.params.id));
+    if (talker) return res.status(200).json(talker);
+    return res.status(404).json(TALKER_NOT_FOUND);
   } catch (error) {
     console.error(error);
   }
